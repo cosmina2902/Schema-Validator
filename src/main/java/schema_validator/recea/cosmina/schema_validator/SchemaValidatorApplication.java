5389bdc65bb5eb852e56d5f23de3ea3cb65cc519
Cosmina <recea.cosmina@gmail.com>
2024-08-04 21:51:48 +0300

    Prepare the workspace

---
+++ src/main/java/schema_validator/recea/cosmina/schema_validator/SchemaValidatorApplication.java
@@ -0,0 +1,13 @@
+package schema_validator.recea.cosmina.schema_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/schema_validator/recea/cosmina/schema_validator/SchemaValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package schema_validator.recea.cosmina.schema_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
